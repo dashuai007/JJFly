397ef6d62ad697fc14bbeb565a282bc757c4cf79
John <zhangshuai11241@163.com>
2016-03-26 16:25:25 +0800

    presentation controller exercise

---
+++ UIPresentationController/UIPresentationController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIPresentationController
+//
+//  Created by John on 16/3/26.
+//  Copyright © 2016年 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIPresentationController/UIPresentationController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIPresentationController
+//
+//  Created by John on 16/3/26.
+//  Copyright © 2016年 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
